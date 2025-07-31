2bdb04e55e7383ebaab304c5aa77baccaaead46d
MSI <tulqinurinov005@gmail.com>
2025-07-31 13:38:09 +0500

    dsfefda

--- config/settings.py
+++ config/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         "USER": config("POSTGRES_USER"),
         "PASSWORD": config("POSTGRES_PASSWORD"),
         "HOST": config("POSTGRES_HOST", default="localhost"),
-        "PORT": config("POSTGRES_PORT", default=5432, cast=int),
+        "PORT": config("POSTGRES_PORT", default=5433, cast=int),
         "OPTIONS": {
             "client_encoding": "UTF8",
         },
