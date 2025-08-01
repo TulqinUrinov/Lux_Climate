2cca4b38bf25dd219d5cd2d82972dda49a767525
MSI <tulqinurinov005@gmail.com>
2025-08-01 17:59:00 +0500

    dfsgd

--- config/settings.py
+++ config/settings.py
@@ -151,8 +151,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'data.user.User'
-
 REST_FRAMEWORK = {
 
     "DEFAULT_AUTHENTICATION_CLASSES": [
