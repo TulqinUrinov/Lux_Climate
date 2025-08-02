56d0ba16bc3312fe369724b996d915312312df25
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-02 16:12:38 +0500

    dfg

--- data/payment/signals.py
+++ data/payment/signals.py
@@ -11,15 +11,4 @@ def on_new_payment(sender, instance: Payment, created: bool, **kwargs):
     if not created:
         return
 
-    Balance.objects.create(
-        customer=instance.customer,
-        user=instance.created_by,
-        amount=instance.amount,
-        reason=(
-            "PAYMENT_INCOME" if instance.payment_type == "INCOME" else "PAYMENT_OUTCOME"
-        ),
-        comment=instance.comment,
-        type="INCOME" if instance.payment_type == "INCOME" else "OUTCOME",
-        change=instance.amount,
-        payment=instance,
-    )
+    instance.customer.recalculate_balance()
