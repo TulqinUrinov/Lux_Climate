a213d4d81bef31829d6617893675deb3177e790d
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-02 16:13:15 +0500

    dfg

--- data/payment/apps.py
+++ data/payment/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class PaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'data.payment'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "data.payment"
+
+    def ready(self):
+        import data.payment.signals
