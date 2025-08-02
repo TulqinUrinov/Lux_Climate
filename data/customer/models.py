6b91809857ed39cd84b0724041ca87b9c189ed70
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-02 15:59:40 +0500

    dfg

--- data/customer/models.py
+++ data/customer/models.py
@@ -21,7 +21,7 @@ class Customer(BaseModel):
     phone_number = models.CharField(max_length=20, unique=True)
     get_order = models.BooleanField(default=False)
 
-    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    balance = models.DecimalField(max_digits=12, decimal_places=2, default=Decimal("0"))
 
     def __str__(self):
         return self.full_name
