b80f8a4770543c754ed61a2f5216535701a1058d
shukurulloxkomiljonov <me@komiljonov.uz>
2025-08-02 16:29:20 +0500

    dfg

--- data/order/apps.py
+++ data/order/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class ProductConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'data.order'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "data.order"
+
+    def ready(self):
+
+        import data.order.signals
