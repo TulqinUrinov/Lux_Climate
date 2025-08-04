98441329f7e2e0bb3bcfcd58b79bfa9728122297
MSI <tulqinurinov005@gmail.com>
2025-08-04 20:26:53 +0500

    daretgrrew

--- data/customer/views.py
+++ data/customer/views.py
@@ -1,6 +1,7 @@
 from typing import cast
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+
+from data.bot.permission import IsBotAuthenticated
 from data.common.pagination import CustomPagination
 from data.customer.serializers import *
 
@@ -12,7 +13,7 @@ from rest_framework.response import Response
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsBotAuthenticated]
     pagination_class = CustomPagination
 
     def get_serializer_context(self):
@@ -20,7 +21,6 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["get", "post"], url_path="recalculate")
     def recalculate(self, request, pk):
-
         customer: "Customer" = cast(Customer, self.get_object())
 
         customer.recalculate_balance()
