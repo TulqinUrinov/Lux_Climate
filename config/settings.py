d222679f9e8bbee0fa5dc62e64191b4131423831
MSI <tulqinurinov005@gmail.com>
2025-08-04 20:16:42 +0500

    daretgrrew

--- config/settings.py
+++ config/settings.py
@@ -169,13 +169,9 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 REST_FRAMEWORK = {
-    #
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
 }
 
 SIMPLE_JWT = {
