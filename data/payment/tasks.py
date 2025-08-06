e6cc6d4854aa7964835702dec456f1d392928d48
MSI <tulqinurinov005@gmail.com>
2025-08-06 12:59:44 +0500

    dfgedea

--- data/payment/tasks.py
+++ data/payment/tasks.py
@@ -1,8 +1,13 @@
 from celery import shared_task
 from django.utils import timezone
 from datetime import timedelta
+from typing import TYPE_CHECKING
+
 from data.payment.send_reminder import send_payment_reminder
 from data.payment.models import InstallmentPayment
+
+if TYPE_CHECKING:
+    from data.bot.models import BotUser
 
 
 @shared_task(bind=True, name='data.payment.tasks.check_upcoming_installments')
@@ -14,7 +19,8 @@ def check_upcoming_installments(self):
 
         for installment in installments:
             customer = installment.customer
-            telegram_id = getattr(getattr(customer, 'bot_user', None), 'chat_id', None)
+            bot_user: "BotUser" = BotUser.objects.filter(customer=customer).first()
+            telegram_id = bot_user.chat_id
 
             if telegram_id:
                 print(f"Processing reminder for Telegram ID: {telegram_id}")
