37117ef5e755ac4ef7e9d769617ca490e9ece0c1
MSI <tulqinurinov005@gmail.com>
2025-08-06 14:40:09 +0500

    dfgedea

--- config/celery_worker.py
+++ config/celery_worker.py
@@ -2,6 +2,8 @@ import os
 from celery import Celery
 from datetime import timedelta
 
+from celery.schedules import crontab
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
@@ -12,9 +14,15 @@ app.autodiscover_tasks(['data.payment'])
 
 # Beat sozlamalari
 app.conf.beat_schedule = {
-    'check-installments-daily': {
+    'check-installments-daily-10am': {
         'task': 'data.payment.tasks.check_upcoming_installments',
-        'schedule': timedelta(minutes=1),
+        'schedule': crontab(hour=10, minute=0),
     },
 }
 
+# app.conf.beat_schedule = {
+#     'check-installments-daily': {
+#         'task': 'data.payment.tasks.check_upcoming_installments',
+#         'schedule': timedelta(minutes=1),
+#     },
+# }
