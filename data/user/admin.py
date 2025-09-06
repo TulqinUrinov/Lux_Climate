cebe2f2e8fad86065bfd8ac448c65553b629f911
MSI <tulqinurinov005@gmail.com>
2025-09-06 12:15:26 +0500

    edfsrd

--- data/user/admin.py
+++ data/user/admin.py
@@ -4,5 +4,5 @@ from data.user.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('full_name','phone_number','is_archived','created_at')
+    list_display = ('full_name','phone_number','is_archived','tg_chat_id','created_at')
     search_fields = ('full_name', 'phone_number',)
