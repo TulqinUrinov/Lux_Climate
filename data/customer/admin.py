24cb5d6907bc59925ea2af8f7fe26ba8c098d19e
MSI <tulqinurinov005@gmail.com>
2025-09-06 12:33:39 +0500

    defretrg

--- data/customer/admin.py
+++ data/customer/admin.py
@@ -4,5 +4,5 @@ from data.customer.models import Customer
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'phone_number', 'is_archived', 'created_at')
+    list_display = ('full_name', 'phone_number', 'tg_chat_id', 'is_archived', 'created_at')
     search_fields = ('full_name', 'phone_number')
